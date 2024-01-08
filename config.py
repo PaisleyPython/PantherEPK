3a65e3693509d15ed8bac9a0a44a0102c949a37a
PaisleyPython <148840962+PaisleyPython@users.noreply.github.com>
2024-01-08 15:49:19 +0000

    create config file to store global var

---
+++ config.py
@@ -0,0 +1,5 @@
+# Config file contains global variable only. This connects the 'client_email' string feild
+# of the invoice generator form in invoice.py, and then allows that variable to be used within
+# mail.py to send invoice PDF direct to the client.
+
+client_email = ""
